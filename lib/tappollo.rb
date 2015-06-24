277314f79d756d4e5a9f5d39f2413e7a165211a1
Zitao Xiong <caoer115@gmail.com>
2015-06-23 23:17:01 -0400

    add template

--- lib/tappollo.rb
+++ lib/tappollo.rb
@@ -3,13 +3,23 @@ require 'erb'
 require 'tilt'
 
 module Tappollo
-  def self.render_table_row
-    template = Tilt.new(File.join(File.dirname(File.expand_path(__FILE__)), '/templates/tableview_builder/TMRowItemTemplate.erb'))
-    output = template.render
-    puts output    
+  def self.render_table_row(class_name, project_name, project_prefix, author_name)
+    generate_files(class_name, project_name, project_prefix, author_name, "RowItem.h", "/templates/tableview_builder/TMRowItemTemplateH.erb")
+    generate_files(class_name, project_name, project_prefix, author_name, "RowItem.m", "/templates/tableview_builder/TMRowItemTemplateM.erb")
+    generate_files(class_name, project_name, project_prefix, author_name, "Cell.h", "/templates/tableview_builder/TMTableViewCellH.erb")
+    generate_files(class_name, project_name, project_prefix, author_name, "Cell.m", "/templates/tableview_builder/TMTableViewCellM.erb")
+    generate_files(class_name, project_name, project_prefix, author_name, "Cell.xib", "/templates/tableview_builder/TMTableViewCellXib.erb")
   end
 
-  def self.path_to_resources
-  File.join(File.dirname(File.expand_path(__FILE__)), '')
-end
+  def self.generate_files(class_name, project_name, project_prefix, author_name, file_postfix, template_path)
+    template = Tilt.new(path_to_resources(template_path))
+    output = template.render(self, :class_name => class_name, :project_name => project_name, :project_prefix => project_prefix, :author_name => author_name)
+    File.open("#{project_prefix}#{class_name}#{file_postfix}", "w") do |file|
+      file.write(output)
+    end
+  end
+
+  def self.path_to_resources(path)
+    File.join(File.dirname(File.expand_path(__FILE__)), path)
+  end
 end
